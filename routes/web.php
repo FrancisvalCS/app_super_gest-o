edf9a1b1f808427abd799f319c0fe259a4990be0
francisval <francisvalcs@gmail.com>
2022-03-24 01:20:56 -0300

    Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos','SobreNosController@sobre-nos')->name('site.sobrenos');
+Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato','ContatoController@contato')->name('site.contato');
 Route::get('/login',function(){return 'Login';})->name('site.login');
 
@@ -25,3 +25,13 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
 
+Route::get('rota1', function(){
+        echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('rota2', function(){
+        return redirect()->route('site.rota1');
+        echo 'Rota 2';
+})->name('site.rota2');
+
+//Route::redirect('/rota2','rota1');
